dc6a4a18073cedb8a7330e4bba44e2797da6a3f7
Albresky <78648318+Albresky@users.noreply.github.com>
2023-05-23 17:42:22 +0800

    add timestamp to final output

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from utils.master import Master
-from utils.time import getNowTimeWithOffset
+from utils.time import getNowTime, getNowTimeWithOffset
 from UserInterface import UserInterface
 from time import sleep
 
 def getInfo(trial,plan,resp):
     info=plan['seatsInfo'][0]
-    print(f"[try={trial}] MSG={resp['MESSAGE']} | {info['roomName']},{info['floorName']},{info['seatNum']}座,{plan['beginTime'].strftime('%Y-%m-%d %H:%M')},{plan['duration']}小时")
+    print(f"[{getNowTime()}][try={trial}] MSG={resp['MESSAGE']} | {info['roomName']},{info['floorName']},{info['seatNum']}座,{plan['beginTime'].strftime('%Y-%m-%d %H:%M')},{plan['duration']}小时")
 
 def run():
     api = UserInterface()
@@ -32,9 +32,9 @@ def run():
                     sleep(delay)
                 tryTimes+=1
             if isSuccess:
-                print(f"[plan={planCode}] 预约成功")
+                print(f"[{getNowTime()}][plan={planCode}] 预约成功")
             else:   
-                print(f"[plan={planCode}] 预约失败")
+                print(f"[{getNowTime()}][plan={planCode}] 预约失败")
             
              
 if __name__ == "__main__":
--- utils/time.py
+++ utils/time.py
@@ -4,24 +4,49 @@ import pytz
 # UTC+8 timezone name
 timezone_name = 'Asia/Shanghai'
 
-def getNowTime():
+'''
+precision scales for @getNowTime
+'''
+precisions={
+    'second': '%Y-%m-%d %H:%M:%S',
+    'minute': '%Y-%m-%d %H:%M',
+    'hour': '%Y-%m-%d %H',
+    'day': '%Y-%m-%d'
+}
+
+
+def getNowTime(toStr=False, precision='second'):
+    ''' Get now time with timezone, default: UTC+8, default precision: second
+    
+    :param toStr: return str or datetime, default: False (return datetime)
+    :param precision: return time with precision, default: 'second', optional: 'minute', 'hour', 'day'
+    '''
     utc_now = datetime.utcnow()
     timezoneCN = pytz.timezone(timezone_name)
     local_now = utc_now.replace(tzinfo=pytz.timezone('UTC')).astimezone(timezoneCN)
+    if toStr:
+        if precisions.get(precision):
+            local_now = local_now.strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            raise Exception(f"precision '{precision}' is not defined")
     return local_now
 
-'''
-get now time with offset(days,hours), precision: hour
-'''
+
 def getNowTimeWithOffset(days=0,hours=0):
+    ''' Get now time with offset(days,hours), precision: minute
+
+    :param days: offset days, default: 0
+    :param hours: offset hours, default: 0
+    :return: now time (datetime) with offset
+    '''
     timeNow = getNowTime()
-    timeNow=timeNow.strftime("%Y-%m-%d %H:%M") # # str(datetime)
+    timeNow = timeNow.strftime("%Y-%m-%d %H:%M")
     timeNow = datetime.strptime(timeNow, "%Y-%m-%d %H:%M")
     if days != 0:
         timeNow += timedelta(days=days)
     if hours != 0:
         timeNow += timedelta(hours=hours)
-    return timeNow # datetime
+    return timeNow
     
 if __name__ == '__main__':
     pass
