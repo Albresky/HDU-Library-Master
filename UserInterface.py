229bf00f99b0afcbc554d1bfd2f4d0871aa7b662
Albresky <78648318+Albresky@users.noreply.github.com>
2023-06-02 13:42:36 +0800

    fix info query failures

--- UserInterface.py
+++ UserInterface.py
@@ -10,7 +10,8 @@ class UserInterface:
     def __init__(self):
         self.configFile = "./config/config.yaml"
         self.master = Master()
-    
+        self.th = None
+        
     def init(self):
         if not os.path.exists(self.configFile):
             print(f"未检测到配置文件，将在config目录下创建配置文件: {self.configFile}")
@@ -22,7 +23,7 @@ class UserInterface:
                 print(f"配置文件解析失败，请检查配置文件是否正确。错误为：")
                 print(e)
                 print(f"若无法解决，请尝试删除{self.configFile}，重新运行程序。")
-                exit(0)
+                self.exit()
             print(f"配置文件解析成功。")
             sleep(1)
 
@@ -40,14 +41,14 @@ class UserInterface:
                 else:
                     print("环境变量中账号密码错误")
                     self.master.delConfigFile()
-                    exit(0)
+                    self.exit()
             else:
-                exit(0)
+                self.exit()
         return flag
     
     
     def exit(self):
-        if self.th.is_alive():
+        if self.th and self.th.is_alive():
             for _ in "请等待其他线程结束...":
                     print(_, end="", flush=True)
                     sleep(0.5 if self.th.is_alive() else 0.1)
@@ -58,7 +59,7 @@ class UserInterface:
         
     def run(self):
         self.init()
-        state=self.login()
+        state = self.login()
         if len(self.master.plans) == 0:
             print("初始化预约方案...")
             if self.addPlan():
@@ -66,7 +67,7 @@ class UserInterface:
                 print("预约方案初始化成功")
             else:
                 print("预约方案初始化失败")
-                exit(0)
+                self.exit()
         return state
 
     def planParser(self, plan:list):
@@ -74,7 +75,7 @@ class UserInterface:
 
     def addPlan(self):
         try:
-            num=0
+            num = 0
             mPlans = self.planParser(self.master.planCode)
             if self.th.is_alive():
                 print("正在初始化楼层和座位信息...")
@@ -88,12 +89,34 @@ class UserInterface:
                     raise Exception("房间类型不合法")
                 roomName = self.master.getRoomNameByIndex(roomName)
                 room = self.master.rooms[roomName]
-                floor = self.master.getFloorNamesByRoom(roomName)
-                if len(floor) == 0:
-                    raise Exception(f"{roomName}没有开放楼层")
-                floorName = self.master.getFloorNameByRoomAndId(roomName, _plan[1]) # floorId2floorName
-                if floorName is None:
-                    raise Exception(f"{roomName}中楼层{_plan[1]}不存在")
+                
+                retries = self.master.job["maxTrials"] + 1
+                delay = self.master.job['delay']
+                floor = None
+                floorName = None
+                while retries >= 0:
+                    retries -= 1
+                    floor = self.master.getFloorNamesByRoom(roomName)
+                    if len(floor) > 0:
+                        floorName = self.master.getFloorNameByRoomAndId(roomName, _plan[1]) # floorId2floorName
+                        if floorName is None:
+                            if retries == 0:
+                                raise Exception(f"{roomName}中楼层{_plan[1]}不存在")
+                            else:
+                                sleep(delay)
+                                self.th = Thread(target=self.master.updateRooms)
+                                self.th.start()
+                                self.th.join()
+                        else:
+                            break
+                    elif len(floor) == 0 and retries != 0:
+                        sleep(delay)
+                        self.th = Thread(target=self.master.updateRooms)
+                        self.th.start()
+                        self.th.join()
+                    else:
+                        raise Exception(f"{roomName}没有开放楼层")
+                    
                 time = getNowTime().replace(hour=_plan[3],minute=0,second=0) # hour
                 if time.hour < room["range"]["minBeginTime"] or time.hour > room["range"]["maxEndTime"]:
                     raise Exception("开始时间不在房间开放时间内")
@@ -122,7 +145,7 @@ class UserInterface:
                 seatBookers = (self.master.uid, )
                 self.master.addPlan(roomName, time, hours, seatsDictList, seatBookers)
                 print(f"plan[{num}]添加成功")
-                num+=1
+                num += 1
             self.master.saveConfig()
             return True
         except Exception as e:
